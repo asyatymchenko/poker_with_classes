20b1b812e686db179ef1410d0d2bd87145570e45
asyatymchenko <53194470+asyatymchenko@users.noreply.github.com>
2019-07-26 08:16:44 +0300

    cleaned

--- combination.rb
+++ combination.rb
@@ -28,13 +28,16 @@ class Combination
   end
 
   def flush
-    if checksuit.max >= 5
-      puts "true"
-    end
+    #if checksuit.max >= 5
+     # puts "true"
+    #end
     return true
   end
 
   def straight
+    if check_sequence.max >= 5
+      puts "true"
+    end
     return true
   end
 
@@ -54,30 +57,43 @@ class Combination
     return true
   end
 
-  def checksuit
+  def check_suit
     suit_counter = Array.new(1,0)
-    suit_iterate = 0
+    iterator = 0
     max_sequence_of_cards = []
+
     sorted_cards = @cards.sort_by { |card| card.suit }
-    sorted_cards.each_with_index { |card, index| index == 0 || card.suit == sorted_cards[index-1].suit ? suit_counter[suit_iterate] += 1 : suit_counter[suit_iterate+=1] = 1 }
-    suit_iterate = 0
-    (0...suit_counter.index(suit_counter.max)).to_a.each {|x| suit_iterate += suit_counter[x]}
-    i = suit_iterate
-    while i < (suit_iterate + suit_counter.max)
+    sorted_cards.each_with_index { |card, index| index == 0 || card.suit == sorted_cards[index-1].suit ? suit_counter[iterator] += 1 : suit_counter[iterator+=1] = 1 }
+
+    iterator = 0
+    (0...suit_counter.index(suit_counter.max)).to_a.each {|x| iterator += suit_counter[x]}
+    i = iterator
+    while i < (iterator + suit_counter.max)
       max_sequence_of_cards << sorted_cards[i]
       i += 1
     end
   end
 
-  def checksequence(cards)
-    cards = cards.uniq
-    cards.sort_by { |card| card.mark }
-    resultarr =[]
-    cards.each_with_index { |card,index | index == 0 || card[index] == card[index-1] + 1 ? resultarr << card : resultarr=[]}
-    resultarr
+  def check_sequence
+    marks_counter = Array.new(1,0)
+    iterator = 0
+    max_sequence_of_cards = []
+
+    sorted_cards = @cards.uniq
+    sorted_cards = sorted_cards.sort_by { |card| card.mark }.reverse
+    sorted_cards.each_with_index { |card,index | index == 0 || card.mark == (sorted_cards[index-1].mark + 1) ? marks_counter[iterator] += 1 : marks_counter[iterator+=1] = 1 }
+
+    iterator = 0
+    (0...marks_counter.index(marks_counter.max)).to_a.each {|x| iterator += marks_counter[x]}
+    i = iterator
+    while i < (iterator + marks_counter.max)
+      max_sequence_of_cards << sorted_cards[i]
+      i += 1
+    end
+    max_sequence_of_cards
   end
 
-  def checkrank(cards)
+  def check_rank(cards)
     repeat_counter = Array.new(1)
     if (cards.uniq).size == cards.size
       return false
